0b4ff660ee103bdc0b616e1f80aa67f9c51b32d0
tareqhasan382 <tareqhasan382@gmail.com>
2023-12-26 02:56:52 +0600

    fix corss policy

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = (): string => {
-  return "https://dev-town-backend.vercel.app";
+  return "https://sky-mart-backend.vercel.app";
 };
 
-// http://localhost:5000 || https://dev-town-backend.vercel.app
+// http://localhost:5000 || https://sky-mart-backend.vercel.app
